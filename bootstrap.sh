21e1c34b29804bb06c70da54e5fc2da708d62319
chrisbarran <chris@Christophers-MacBook-Pro.local>
2014-06-07 19:26:29 +0100

    Add Oracle JVM and map ports to hosts.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,20 +1,32 @@
 #!/usr/bin/env bash
 
-wget -O - http://packages.elasticsearch.org/GPG-KEY-elasticsearch | sudo apt-key add -
+wget -O - http://packages.elasticsearch.org/GPG-KEY-elasticsearch |  apt-key add -
  
-echo "deb http://packages.elasticsearch.org/elasticsearch/1.0/debian stable main" | sudo tee -a /etc/apt/sources.list
+echo "deb http://packages.elasticsearch.org/elasticsearch/1.0/debian stable main" |  tee -a /etc/apt/sources.list
 
-echo "deb http://packages.elasticsearch.org/logstash/1.3/debian stable main" | sudo tee -a /etc/apt/sources.list
-
-sudo apt-get update
+echo "deb http://packages.elasticsearch.org/logstash/1.3/debian stable main" |  tee -a /etc/apt/sources.list
 
 export DEBIAN_FRONTEND=noninteractive
 
-sudo apt-get -q -y install openjdk-7-jdk
+apt-get install python-software-properties
 
-sudo apt-get install elasticsearch
+add-apt-repository -y ppa:webupd8team/java
 
-sudo update-rc.d elasticsearch defaults 95 10
+echo debconf shared/accepted-oracle-license-v1-1 select true |  debconf-set-selections
 
-sudo /etc/init.d/elasticsearch start
+echo debconf shared/accepted-oracle-license-v1-1 seen true |  debconf-set-selections
+
+apt-get update
+
+apt-get install -y oracle-java7-installer
+
+apt-get install elasticsearch
+
+update-rc.d elasticsearch defaults 95 10
+
+/etc/init.d/elasticsearch start
+
+echo -e "\n\nJAVA_HOME=/usr/lib/jvm/java-7-oracle" >> /etc/environment;
+
+export JAVA_HOME=/usr/lib/jvm/java-7-oracle/ 
  
